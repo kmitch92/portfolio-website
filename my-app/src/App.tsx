adce6f366082358767ac54c1ccd922b0b11d9f77
kiel <kielmitchell8@gmail.com>
2022-09-28 17:55:03 +0100

    adds navbar and links

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,23 +4,27 @@ import { Container } from 'react-bootstrap';
 import { Home } from './Pages/Home';
 import { CV } from './Pages/CV';
 import { Portfolio } from './Pages/Portfolio';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
-    <Container>
-      <h1>Kiel's Portfolio Page</h1>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          Home
-        </Route>
-        <Route path="/portfolio" element={<Portfolio />}>
-          Portfolio
-        </Route>
-        <Route path="/cv" element={<CV />}>
-          CV
-        </Route>
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <h1>Kiel's Portfolio Page</h1>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            Home
+          </Route>
+          <Route path="/portfolio" element={<Portfolio />}>
+            Portfolio
+          </Route>
+          <Route path="/cv" element={<CV />}>
+            CV
+          </Route>
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ my-app/src/Components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Button, Container, Nav, Navbar as NavBS } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <NavBS sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/portfolio" as={NavLink}>
+            Portfolio
+          </Nav.Link>
+          <Nav.Link to="/cv" as={NavLink}>
+            CV
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </NavBS>
+  );
+};
