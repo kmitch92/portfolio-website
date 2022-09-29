808b8e216497af1a86aca2dd1e3fe20ae9ba5798
kiel <kielmitchell8@gmail.com>
2022-09-29 15:22:34 +0100

    adds project.json with mixed real and mock data to populate portfolio page. adds auto-generated cards on portfolio page for each item in projects.json

--- my-app/src/Pages/Portfolio.tsx
+++ my-app/src/Pages/Portfolio.tsx
@@ -1,9 +1,31 @@
-import { Container } from 'react-bootstrap';
+import { Container, Card, ListGroup } from 'react-bootstrap';
+import Projects from '../projects.json';
 
 export const Portfolio = () => {
   return (
     <Container>
       <h1>Portfolio Page</h1>
+      {Projects &&
+        Projects.Projects.map((project) => {
+          return (
+            <Card style={{ width: '50rem', marginBottom: '50px' }}>
+              <Card.Img variant="top" src={project.Images[0]} />
+              <Card.Body>
+                <Card.Title>{project.Title}</Card.Title>
+                <Card.Text>{project.Description}</Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                {project.Tags.split(' ').map((tag) => (
+                  <ListGroup.Item>{tag}</ListGroup.Item>
+                ))}
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href={project.Link}>Hosted Link</Card.Link>
+                <Card.Link href={project.GitHub}>Repository</Card.Link>
+              </Card.Body>
+            </Card>
+          );
+        })}
     </Container>
   );
 };
