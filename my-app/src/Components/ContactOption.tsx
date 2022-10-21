f87d6594efcc7d8e238e1b938e8c8c3dc0da2891
kiel <kielmitchell8@gmail.com>
2022-10-21 15:47:32 +0100

    adds contact option links

---
+++ my-app/src/Components/ContactOption.tsx
@@ -0,0 +1,50 @@
+import { Card, Button } from 'react-bootstrap';
+import { FC } from 'react';
+import { BsWhatsapp, BsMessenger, BsEnvelopeFill } from 'react-icons/bs';
+
+export interface Props {
+  option: {
+    icon: string;
+    name: string;
+    contact: string;
+    link: string;
+  };
+}
+
+export const ContactOption: FC<Props> = ({ option }) => {
+  return (
+    <Card
+      style={{
+        margin: '2rem',
+        width: '100%',
+        textAlign: 'center',
+        height: '12rem',
+      }}
+      className="bg-ligher-tg text-darker-tg shadow-sm"
+    >
+      <Card.Header className="text-lighter-tg bg-darker-tg">
+        {option.icon === 'email' && (
+          <BsEnvelopeFill style={{ fontSize: '1.4rem', margin: '0.6rem' }} />
+        )}
+        {option.icon === 'messenger' && (
+          <BsMessenger style={{ fontSize: '1.4rem', margin: '0.6rem' }} />
+        )}
+        {option.icon === 'whatsapp' && (
+          <BsWhatsapp style={{ fontSize: '1.4rem', margin: '0.6rem' }} />
+        )}
+        <h5>{option.name}</h5>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text className="h6">{option.contact}</Card.Text>
+      </Card.Body>
+      <Button
+        href={option.link}
+        className="shadow-sm"
+        variant="outline-darker-tg"
+        style={{ width: '50%', alignSelf: 'center', marginBottom: '0.6rem' }}
+      >
+        Get in Touch!
+      </Button>
+    </Card>
+  );
+};
--- my-app/src/Pages/Contact.tsx
+++ my-app/src/Pages/Contact.tsx
@@ -1,6 +1,70 @@
 import { Card, Container, Form, Button } from 'react-bootstrap';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+
+import { ContactOption, Props } from '../Components/ContactOption';
+
+import { useRef, MutableRefObject, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+const popover = (
+  <Popover id="popover-basic" className="shadow">
+    <Popover.Header
+      className="text-darker-tg"
+      style={{ paddingTop: '1.4rem', border: 'none' }}
+    >
+      <h6>Thanks for reaching out.</h6>
+      <h6> I'll get right back to you!</h6>
+    </Popover.Header>
+  </Popover>
+);
 
 export const Contact = () => {
+  const form: MutableRefObject<any> = useRef();
+  const [showSubmit, toggleShowSubmit] = useState(true);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    toggleShowSubmit(false);
+
+    emailjs
+      .sendForm(
+        'service_bf14raj',
+        'template_ht1x4en',
+        form.current,
+        'ufOr0j4EXDL1S9kQ2'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  const opt1: Props['option'] = {
+    icon: 'email',
+    name: 'Email',
+    contact: 'kielmitchell8@gmail.com',
+    link: 'mailto:kielmitchell8@gmail.com',
+  };
+  const opt2: Props['option'] = {
+    icon: 'messenger',
+    name: 'Messenger',
+    contact: 'Kiel Mitchell',
+    link: 'https://m.me/kiel.mitchell.7',
+  };
+  const opt3: Props['option'] = {
+    icon: 'whatsapp',
+    name: 'WhatsApp',
+    contact: '07950 667413',
+    link: 'https://api.whatsapp.com/send?phone=07950667413',
+  };
+
+  const contOpts = [opt1, opt2, opt3];
+
   return (
     <Container>
       <Card
@@ -27,7 +91,7 @@ export const Contact = () => {
           className="shadow-sm"
         >
           <h1 className="text-darker-tg">Contact Me</h1>
-          <Form>
+          <Form style={{ marginTop: '2rem' }} ref={form} onSubmit={sendEmail}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Control
                 type="email"
@@ -35,9 +99,6 @@ export const Contact = () => {
                 style={{ border: 'none' }}
                 placeholder="Your Email Address"
               />
-              {/* <Form.Text className="text-muted ">
-                This remains between you and me.
-              </Form.Text> */}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicTopic">
@@ -47,9 +108,6 @@ export const Contact = () => {
                 style={{ border: 'none' }}
                 placeholder="Topic"
               />
-              {/* <Form.Text className="text-muted">
-                Let me know what you want to talk about.
-              </Form.Text> */}
             </Form.Group>
             <Form.Group>
               <Form.Control
@@ -58,20 +116,50 @@ export const Contact = () => {
                 style={{ border: 'none' }}
                 placeholder="Message"
               />
-              {/* <Form.Text className="text-muted">
-                Let's get down to business.
-              </Form.Text> */}
             </Form.Group>
-            <Button
-              variant="darker-tg"
-              style={{ marginTop: '20px', border: 'none', borderRadius: '9px' }}
-              className="shadow-sm"
-              type="submit"
-            >
-              Submit
-            </Button>
+            <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+              {showSubmit ? (
+                <Button
+                  variant="darker-tg"
+                  style={{
+                    marginTop: '20px',
+                    border: 'none',
+                    borderRadius: '9px',
+                  }}
+                  className="shadow-sm"
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              ) : (
+                <Button
+                  variant="darker-tg"
+                  style={{
+                    marginTop: '20px',
+                    border: 'none',
+                    borderRadius: '9px',
+                  }}
+                  className="shadow-sm"
+                  disabled
+                >
+                  Submit
+                </Button>
+              )}
+            </OverlayTrigger>
           </Form>
         </Card>
+        <Container
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: '1rem',
+          }}
+        >
+          {contOpts.map((option) => (
+            <ContactOption option={option} />
+          ))}
+        </Container>
       </Card>
     </Container>
   );
